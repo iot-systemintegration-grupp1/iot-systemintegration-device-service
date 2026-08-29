d3c58ef53c9f2877816d5e51ba08151fd359756b
Fredrik <tupplur_bloggarna.0w@icloud.com>
2026-08-29 23:27:52 +0200

    Add initial device authorization endpoint

--- DeviceService/Program.cs
+++ DeviceService/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/health", () => "Device Service är igång!");
+app.MapGet("/internal/devices/{deviceId}/authorization",
+    (string deviceId) => {
+        if (deviceId == "DEVICE-001")
+        {
+            return "The device is authorized!";
+        }
+        else
+        {
+            return "The device is not authorized!";
+        }
+    });
+
+
+
 
 app.Run();
