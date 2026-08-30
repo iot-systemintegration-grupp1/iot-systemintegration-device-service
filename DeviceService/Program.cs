2fa2b2ce1beb80c090d33e9da27968e91050878a
Fredrik <tupplur_bloggarna.0w@icloud.com>
2026-08-30 23:03:22 +0200

    Move authorization logic to service

--- DeviceService/Program.cs
+++ DeviceService/Program.cs
@@ -1,14 +1,18 @@
+using DeviceService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 List<string> authorizedDevices = new List<string> { "DEVICE-001", "TEMP-001", "HUMIDITY-001"};
 
+DeviceAuthorizationService authorizationService = new DeviceAuthorizationService();
+
 app.MapGet("/health", () => "Device Service är igång!");
 app.MapGet("/internal/devices/{deviceId}/authorization",
     (string deviceId) => {
 
-        bool isAuthorized = authorizedDevices.Contains(deviceId);
+        bool isAuthorized = authorizationService.IsAuthorized(deviceId, authorizedDevices);
 
         var response = new { deviceId = deviceId, authorized = isAuthorized };
 
