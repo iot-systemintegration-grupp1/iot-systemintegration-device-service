898ed0cfeac3128517215fccec8a5a1526faa847
Fredrik <tupplur_bloggarna.0w@icloud.com>
2026-08-30 13:45:09 +0200

    Return JSON authorization response

--- DeviceService/Program.cs
+++ DeviceService/Program.cs
@@ -2,19 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+List<string> authorizedDevices = new List<string> { "DEVICE-001", "TEMP-001", "HUMIDITY-001"};
+
 app.MapGet("/health", () => "Device Service är igång!");
 app.MapGet("/internal/devices/{deviceId}/authorization",
     (string deviceId) => {
-        if (deviceId == "DEVICE-001")
-        {
-            return "The device is authorized!";
-        }
-        else
-        {
-            return "The device is not authorized!";
-        }
-    });
 
+        bool isAuthorized = authorizedDevices.Contains(deviceId);
+
+        var response = new { deviceId = deviceId, authorized = isAuthorized };
+
+        return response;
+ 
+    });
 
 
 
